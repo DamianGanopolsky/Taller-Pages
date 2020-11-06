3c82705704d3ef5da3cd05b56f3fb581903a98f0
DamianGanopolsky <damian98dgg@gmail.com>
2020-11-06 16:51:59 -0300

    Elimino codigo repetido en Parser

--- server_src/Parser.cpp
+++ server_src/Parser.cpp
@@ -5,33 +5,25 @@
 #define HEADER_LINE 2
 #define BODY_LINE 3
 
-//void Obtener_Recurso(std::string& linea,std::string )
+void Obtener_Key_Recurso(std::string& linea,std::string& recurso){
+	std::string subline_res=linea.substr(linea.find('/'));
+	if(subline_res.at(1)==' '){
+		std::cout << "Recurso root" << std::endl;
+		recurso="/";
+	}
+	else{
+		recurso=subline_res.substr(1,subline_res.find(' '));
+		std::cout << recurso << std::endl;
+	}
+}
 
 void Obtener_Cabecera(std::string& linea,std::string& tipo_petitorio,std::string& resource){
 	if (linea.find("GET")!= std::string::npos){
 		tipo_petitorio="GET";
-		std::string subline_res=linea.substr(linea.find('/'));
-		if(subline_res.at(1)==' '){
-			std::cout << "Recurso root" << std::endl;
-			resource="/";
-		}
-		else{
-			resource=subline_res.substr(1,subline_res.find(' '));
-			std::cout << resource << std::endl;
-		}
-
+		Obtener_Key_Recurso(linea,resource);
 	}else if (linea.find("POST")!= std::string::npos){
 		tipo_petitorio="GET";
-		std::string subline_res=linea.substr(linea.find('/'));
-		if(subline_res.at(1)==' '){
-			std::cout << "Recurso root" << std::endl;
-			resource="/";
-		}
-		else{
-			resource=subline_res.substr(1,subline_res.find(' '));
-			std::cout << resource << std::endl;
-		}
-
+		Obtener_Key_Recurso(linea,resource);
 	}else{
 
 	}
@@ -42,7 +34,6 @@ std::tuple<std::string,std::string,std::string> Parser::Parsear_Archivo(){
 	std::tuple <std::string,std::string,std::string> datos_petitorio;
 	std::string tipo_de_petitorio,resource,body,line;
 	std::istringstream iss(texto);
-	//int numero_de_linea=0;
 	int tipo_de_linea=FIRST_LINE;
 
 	while (std::getline(iss, line)){
@@ -58,15 +49,11 @@ std::tuple<std::string,std::string,std::string> Parser::Parsear_Archivo(){
 		if(tipo_de_linea==BODY_LINE){
 			body.append(line);
 		}
-		//std::cout << line << std::endl;
-		//numero_de_linea++;
 	}
 	std::cout << "Tipo" << tipo_de_petitorio << std::endl;
 	std::cout << "Recurso" << resource << std::endl;
 	std::cout << "Body" << body << std::endl;
-
 	return datos_petitorio;
-
 }
 
 Parser::~Parser(){
